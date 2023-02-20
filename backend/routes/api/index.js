a98a7aaec67b117732d0e82c777ee3e0e038dead
spencerwilf <spencerwilf@yahoo.com>
2023-02-20 14:53:26 -0800

    more db work

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,8 +9,8 @@ const { restoreUser } = require("../../utils/auth.js");
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
+router.use('/spots', spotsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
