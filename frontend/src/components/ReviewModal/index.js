f1ed1839df69df53d8b2af347ef4de86d2d64508
spencerwilf <spencerwilf@yahoo.com>
2023-03-19 14:01:18 -0700

    fin

--- frontend/src/components/ReviewModal/index.js
+++ frontend/src/components/ReviewModal/index.js
@@ -16,14 +16,17 @@ const ReviewModal = ({spotId}) => {
     const { closeModal } = useModal();
 
 
-
     const handleSubmit = (e) => {
         e.preventDefault();
+        let validationErrors = {};
+        if (review.length > 254) {
+          validationErrors.review = 'Review must be less than 255 characters'
+          setErrors(validationErrors)
+          return alert('Please fix errors before submitting');
+        }
         // setIsSubmitted(true);
 
-        if (Object.values(errors).length > 0) {
-            return alert('Please fix errors before submitting');
-        }
+
 
         const newReview = {
             review,
@@ -41,12 +44,9 @@ const ReviewModal = ({spotId}) => {
     <div className='review-modal-wrapper-div'>
     <div id='review-modal'>
       <h2 className='review-box-header'>How was your stay?</h2>
+
       <form onSubmit={handleSubmit} className='leave-review-form'>
-        <ul>
-          {Object.values(errors).map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
+
         <div>
   <label htmlFor='review'></label>
   <textarea
@@ -57,6 +57,8 @@ const ReviewModal = ({spotId}) => {
     onChange={e => setReview(e.target.value)}
   />
 </div>
+
+{errors.review && (<p className='creation-errors'>{errors.review}</p>)}
 
 <div className='review-modal-stars-text'>
   <ReviewRating rating={rating} setRating={setRating}/> stars</div>
